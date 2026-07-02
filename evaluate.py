366651c6e66f83c5c63dce10a3348fa587294f02
CODERS20 <62946665+CODERS20@users.noreply.github.com>
2026-07-02 23:57:11 +0530

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -29,15 +29,13 @@ class increase_contrast(object):
     def __call__(self, img):
         return TF.adjust_contrast(img, contrast_factor=1.8)
 
-transform = transforms.Compose([
-    transforms.Resize((256,256)),
+test_transform = transforms.Compose([
+    transforms.Resize((256, 256)),
     increase_contrast(),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(degrees=180),
     transforms.ToTensor()
 ])
 
-dataset = datasets.ImageFolder(root = data_directory,transform = transform)
+dataset = datasets.ImageFolder(root = data_directory,transform = None)
 
 #train test validation
 
@@ -52,55 +50,78 @@ train_set,val_set,test_set = random_split(
     generator=torch.Generator().manual_seed(40)
 )
 
-train_loader = DataLoader(train_set,batch_size=32,shuffle=True,num_workers=2,pin_memory=True)
-val_loader = DataLoader(val_set,batch_size=32,shuffle=False)
-test_loader = DataLoader(test_set,batch_size=32,shuffle=False)
+class transform_subset(Dataset):
+  def __init__(self,subset,transform):
+    self.subset = subset
+    self.transform = transform
+
+  def __getitem__(self,index):
+    img,label = self.subset[index]
+
+    if self.transform:
+      img = self.transform(img)
+
+    return img,label
+
+  def __len__(self):
+    return len(self.subset)
+
+modified_train = transform_subset(train_set,train_transform)
+modified_val = transform_subset(val_set,test_transform)
+modified_test = transform_subset(test_set,test_transform)
+
+train_loader = DataLoader(modified_train,batch_size=32,shuffle=True,num_workers=2,pin_memory=True)
+val_loader = DataLoader(modified_val,batch_size=32,shuffle=False)
+test_loader = DataLoader(modified_test,batch_size=32,shuffle=False)
 
 def ResNet50(img_channels=3, num_classes = 5):
   return ResNet(block,[3,4,6,3],img_channels,num_classes)
 model = ResNet50(img_channels=3, num_classes=5).to(device)
 
-correct = 0
-total = 0
+model.load_state_dict(torch.load('trained_net.pth', map_location=device))
+
+from sklearn.metrics import classification_report, accuracy_score
+
+predict_list = []
+label_list = []
 
 model.eval()
+
 
 with torch.no_grad():
   for data in test_loader:
-    images,labels = data
-
+    images, labels = data
     images = images.to(device)
     labels = labels.to(device)
 
+    #Forward pass and makes predictions
     outputs = model(images)
     _,predicted = torch.max(outputs,1)
-    total += labels.size(0)
-    correct += (predicted ==labels).sum().item()
-accuracy = 100*correct/total
-print(f'Accuracy score:{accuracy}')
 
+    predict_list.extend(predicted.cpu().numpy())
+    label_list.extend(labels.cpu().numpy())
 
+prediction_arr = np.array(predict_list)
+label_arr = np.array(label_list)
 
-all_preds = []
-all_labels = []
+acc = accuracy_score(label_arr,prediction_arr)*100
 
-model.eval()
-with torch.no_grad():
-    for images, labels in test_loader:
-        images = images.to(device)
-        outputs = model(images)
-        _, predicted = torch.max(outputs, 1)
-        all_preds.extend(predicted.cpu().numpy())
-        all_labels.extend(labels.numpy())
+rpt = classification_report(label_arr,prediction_arr,target_names=classes,digits=4)
 
-cm = confusion_matrix(all_labels, all_preds)
+print(f'Test Set Accuracy = {acc:.2f}%')
+print('Classification Report')
+print(rpt)
+
+#generating confusion matrix
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+cm = confusion_matrix(label_arr, prediction_arr, labels=list(range(len(classes))))
 plt.figure(figsize=(8, 6))
-sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
-            xticklabels=dataset.classes,
-            yticklabels=dataset.classes)
-plt.savefig('/content/confusion_matrix.png', dpi=300, bbox_inches='tight')
 
-plt.xlabel('Predicted Objects')
-plt.ylabel('True Objects')
+sns.heatmap(cm, annot = True, fmt = 'd',cmap='Blues', xticklabels = classes,yticklabels=classes)
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
 plt.title('Confusion Matrix')
+plt.tight_layout()
 plt.show()
